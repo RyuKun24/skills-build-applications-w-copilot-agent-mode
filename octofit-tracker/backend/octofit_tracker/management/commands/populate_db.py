61ef8335cd7eff14d30c8ddac8859286e655d828
RyuKun24 <oezaragoza@gmail.com>
2026-04-15 23:28:01 +0000

    Agregar estructura básica del proyecto Django con modelos, vistas y configuración de API

---
+++ octofit-tracker/backend/octofit_tracker/management/commands/populate_db.py
@@ -0,0 +1,48 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+from djongo import models
+
+from octofit_tracker import models as octo_models
+
+class Command(BaseCommand):
+    help = 'Populate the octofit_db database with test data'
+
+    def handle(self, *args, **options):
+        # Eliminar datos existentes de cada colección usando delete() sobre el queryset
+        User = get_user_model()
+        # Limpiar colecciones usando la API de bajo nivel de Djongo/MongoDB
+        from django.db import connection
+        db = connection.cursor().db_conn.client[connection.settings_dict['NAME']]
+        db['octofit_tracker_user'].drop()
+        db['octofit_tracker_team'].drop()
+        db['octofit_tracker_activity'].drop()
+        db['octofit_tracker_leaderboard'].drop()
+        db['octofit_tracker_workout'].drop()
+
+        # Crear equipos
+        marvel = octo_models.Team.objects.create(name='Marvel')
+        dc = octo_models.Team.objects.create(name='DC')
+
+        # Crear usuarios
+        ironman = User.objects.create_user(username='ironman', email='ironman@marvel.com', password='1234', team=marvel)
+        spiderman = User.objects.create_user(username='spiderman', email='spiderman@marvel.com', password='1234', team=marvel)
+        batman = User.objects.create_user(username='batman', email='batman@dc.com', password='1234', team=dc)
+        superman = User.objects.create_user(username='superman', email='superman@dc.com', password='1234', team=dc)
+
+        # Crear actividades
+        octo_models.Activity.objects.create(user=ironman, type='run', duration=30, distance=5)
+        octo_models.Activity.objects.create(user=spiderman, type='cycle', duration=45, distance=20)
+        octo_models.Activity.objects.create(user=batman, type='swim', duration=25, distance=1)
+        octo_models.Activity.objects.create(user=superman, type='run', duration=60, distance=10)
+
+        # Crear leaderboard
+        octo_models.Leaderboard.objects.create(user=ironman, points=100)
+        octo_models.Leaderboard.objects.create(user=spiderman, points=80)
+        octo_models.Leaderboard.objects.create(user=batman, points=90)
+        octo_models.Leaderboard.objects.create(user=superman, points=110)
+
+        # Crear workouts
+        octo_models.Workout.objects.create(name='Full Body', description='Entrenamiento completo', duration=60)
+        octo_models.Workout.objects.create(name='Cardio', description='Entrenamiento cardiovascular', duration=45)
+
+        self.stdout.write(self.style.SUCCESS('La base de datos octofit_db ha sido poblada con datos de prueba.'))
